5c6913ec5925407f06bf410c75bb971fd721f191
Nahxo00 <diego.aguayo12@gmail.com>
2024-06-27 01:46:56 -0400

    Actualizacion de referencias

--- cliente.php
+++ cliente.php
@@ -72,7 +72,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </svg>
                 Clientes
             </a>
-            <a href="#" class="flex items-center gap-2 px-4 py-2 rounded-lg text-gray-600 hover:bg-gray-300 hover:no-underline">
+            <a href="./cotizaciones.php" class="flex items-center gap-2 px-4 py-2 rounded-lg text-gray-600 hover:bg-gray-300 hover:no-underline">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-5 w-5">
                     <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
                     <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
@@ -82,7 +82,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 </svg>
                 Cotizaciones
             </a>
-            <a href="#" class="flex items-center gap-2 px-4 py-2 rounded-lg text-gray-600 hover:bg-gray-300 hover:no-underline">
+            <a href="./inventario.php" class="flex items-center gap-2 px-4 py-2 rounded-lg text-gray-600 hover:bg-gray-300 hover:no-underline">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-5 w-5">
                     <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
                     <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
@@ -98,7 +98,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <header class="flex items-center justify-between border-b pb-4 mb-6">
             <h1 class="text-2xl font-semibold">Clientes</h1>
             <div class="flex items-center gap-2">
-                <input type="text" placeholder="Buscar clientes..." class="border rounded-md px-4 py-2">
+                <input type="text" placeholder="Buscar clientes..." class="w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             </div>
         </header>
         <button class="bg-blue-500 hover:bg-blue-700 text-white p-1.5 border border-blue-700 rounded mb-2" id="abrirClientes">
@@ -128,7 +128,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             <td class="px-6 py-4 whitespace-nowrap text-center"><?php echo $data['telefono']; ?></td>
                             <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                 <form method="post" action="clases/eliminar.php?accion=pro&id=<?php echo $data['id_cliente']; ?>" class="inline">
-                                    <button class="bg-red-600 p-2.5 rounded hover:bg-red-800 eliminar" type="submit"><i class="fas fa-trash fa-lg" style="color: white;"></i></button>    
+                                    <button class="bg-red-600 p-2.5 rounded hover:bg-red-800 eliminar" type="submit"><i class="fas fa-trash fa-lg" style="color: white;"></i></button>
+
                                 </form>
                                 <button class="bg-green-600 p-2.5 rounded hover:bg-green-800" style="color: white;" type="button" onclick="editar(<?php echo $data['id_cliente']; ?>)">
                                     <i class="fa-regular fa-pen-to-square "></i>
@@ -216,6 +217,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </div>
 </div> 
 </body>
+
+<div id="borrar-cliente" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-md max-h-full">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="p-4 md:p-5 text-center">
+                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                </svg>
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
+                <button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                    Yes, I'm sure
+                </button>
+                <button data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <script>
     $(document).ready(function() {
